41f73fa8180e141df58a87c4abb2ff505b5b7717
Ndi Tigana~ <nditigana01@gmail.com>
2023-02-28 14:26:00 +0100

    adding more functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import Form from './Components/Form'
+import Form from './Components/Form';
+import React, { useState, useEffect } from 'react';
+import CircleLoader from 'react-spinners/CircleLoader'
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 6000)
+  }, []);
+
+
   return (
     <div className="App">
-      <Form></Form>
+      {
+        loading ? (
+
+          <CircleLoader size={150} color={"#eaee1a"} loading={loading} />
+        ) :
+
+          <Form>
+
+          </Form>
+      }
     </div>
   );
+
 }
+
 
 export default App;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  image from '../image/pexels-pixabay-302743.jpeg';
+import  bgImg from '../image/pexels-pixabay-302743.jpeg';
 import {useForm} from 'react-hook-form';
 
 export default function Form() {
@@ -7,27 +7,31 @@ export default function Form() {
 const{register,handleSubmit,watch,formState:{errors}}=useForm()
 const onSubmit=data=>console.log(data);
 
-// console.log(watch('username'));
+ console.log(watch('username'));
   return (
     <section>
         <div className='register'>
             <div className='col-1'>
-                <h2>Sign In</h2>
+                <h2>Create Your Account</h2>
                 <span>Register and Enjoy Our Services</span>
 
                 <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <input type='text' {...register('username')} placeholder ='username or email'/>
-                    <input type='text' {...register('password')} placeholder ='password'/>
+                    <input type='text' {...register('username',{required:true})} placeholder ='username or email'/>
+                    <input type='text' {...register('password',{required:true})} placeholder ='password'/>
                     <input type='text' {...register('confirmpwd')} placeholder ='confirm password'/>
                     <input type='text' {...register("mobile", {required : true, maxLength: 10})} placeholder ='mobile number'/>
                     {errors.mobile?.type === "required" && "Mobile Number is required"}
                     {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
+                    <br/>
+                    {errors.username?.type === "required" && "Fill the blank to proceed!"}
+                    <br/>
+                    {errors.password?.type === "required" && "Password is required!"}
                     <button className='btn'>Sign In</button>
                 </form>
             </div> 
 
              <div className='col-2'>
-                <img src={image} alt='image'/>
+                <img src={bgImg} alt='image'/>
              </div>
         </div>
     </section>
